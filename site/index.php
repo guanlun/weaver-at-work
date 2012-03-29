02337e3f88c59e836f71c9d383d37e35c34844b2
Guanlun ZHAO <guanlunzhao@gmail.com>
2012-03-29 19:23:51 +0800

    graphical design

---
+++ site/index.php
@@ -0,0 +1,24 @@
+<?php
+    include('header.php');
+?>
+           <div id='content_container'>
+                <div class='entry'>
+                    <div class='entry_title'><a href='articles/page_resize.php'>Practical Skills for Dealing with Webpage Resize</a></div>
+                    <div class='entry_category'><a href='#'>Web Design</a></div>
+                </div>
+                <div class='entry'>
+                    <div class='entry_title'><a href='#'>MSF-Go -- An Android App for Médecins Sans Frontières (MSF)</a></div>
+                    <div class='entry_category'><a href='#'>Android Development</a></div>
+                </div>
+                <div class='entry'>
+                    <div class='entry_title'><a href='#'>Implementation of the Mosaic Effect</a></div>
+                    <div class='entry_category'><a href='#'>Computer Graphics</a></div>
+                </div>
+                <div class='entry'>
+                    <div class='entry_title'><a href='projects/bl-dg.php'>BL-DG Website</a></div>
+                    <div class='entry_category'><a href='#'>Web Design</a></div>
+                </div>
+            </div>
+<?
+    include('footer.php');
+?>
---
+++ waw/scripts/index.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('img').load(function() {
+        var body_width = $(window).width();
+        var body_height = $(window).height();
+
+        var ctn_width = $('#container').width();
+        var margin_left = (body_width - ctn_width) / 2;
+        $('#container').css('left', margin_left);
+
+        var contact_width = $('#contact_view').width();
+        var contact_height = $('#contact_view').height();
+
+        $('#contact_view').css('left', body_width - contact_width - 10);
+        $('#contact_view').css('top', body_height - contact_height - 10);
+    });
+});
