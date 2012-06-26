c66082100026daefc60ef9b85516a1634126f1db
guanlun <guanlunzhao@gmail.com>
2012-06-26 23:53:56 +0800

    project page added

---
+++ waw/index.php
@@ -0,0 +1,19 @@
+<? include_once('templates/header.php'); ?>
+        <link href="/css/index.css" rel="stylesheet" />
+        <div id='container'>
+            <div id='weaver_container'>
+                <img src='images/weaver-wire.png'>
+            </div>
+            <div id='dirty_container'>
+                talking's cheap, get your hands dirty
+            </div>
+            <div id='title_container'>
+                <!--
+                <img src='images/title_p.png'>
+                -->
+                <div id='weaver_text'>weaver</div>
+                <div id='at_work_text'>at work</div>
+            </div>
+            <? include_once('templates/nav_bar.php'); ?>
+        </div>
+<? include_once('templates/footer.php'); ?>
---
+++ waw/texts/index.php
@@ -0,0 +1,12 @@
+<? include_once('../templates/header.php'); ?>
+        <link href="../css/texts.css" rel="stylesheet" />
+        <div id='container'>
+        	<? include_once('../templates/nav_bar.php'); ?>
+	
+            <div id='texts_list_container'>
+            	<div class='code'>
+            		public static void main
+            	</div>
+            </div>
+        </div>
+<? include_once('../templates/footer.php'); ?>
