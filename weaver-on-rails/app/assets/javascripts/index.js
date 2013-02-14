3f8d3f0945aa35ec71ae5926c4cc753e237fea0c
guanlun <guanlunzhao@gmail.com>
2013-02-14 20:12:08 +0800

    deploy

--- weaver-on-rails/app/assets/javascripts/index.js
+++ weaver-on-rails/app/assets/javascripts/index.js
@@ -1,8 +1,5 @@
 $(function() {
     $('img').load(function() {
         $('#weaver_container').addClass('weaver_move_up');
-        $('#banner_contact_part').click(function() {
-          $('#talkbubble').css('display', 'block'); 
-        });
     });
 });
