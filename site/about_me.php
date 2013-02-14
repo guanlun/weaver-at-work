ec496bc06b438464206623f580cad8607e7abcd0
guanlun <guanlunzhao@gmail.com>
2013-02-14 21:26:21 +0800

    deployed

--- site/about_me.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include('header.php');
-?>
-            <div id='content_container'>
-            </div>
-<?php
-    include('footer.php');
-?>
--- test/scripts/index.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    var body_width = $(window).width();
-    var body_height = $(window).height();
-
-    var ctn_width = $('#container').width();
-    $('#container').css('margin-left', (body_width - ctn_width) / 2);
-});
